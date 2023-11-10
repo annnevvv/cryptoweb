d17d220c395ff695b7d8ab74724fdb8588716e40
Anna Walaszek <noreplay@annn.evvv>
2023-11-10 22:57:58 +0100

    added user profile nad transaction diarys models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class UserAccountProfile(models.Model):
+    users = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.name
+
+
+class TransactionDiary(models.Model):
+    list_of_tr = models.CharField(0)
