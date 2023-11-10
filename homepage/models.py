82f1dfde830852d25f72e2db714dc592e63e1cce
Anna Walaszek <noreplay@annn.evvv>
2023-11-10 22:46:19 +0100

    added model CryptocurrencyExchange

--- homepage/models.py
+++ homepage/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class CryptocurrencyExchange(models.Model):
+    title = models.CharField()
+    country = models.CharField()
+    image = models.ImageField()
+    trust_score = models.PositiveIntegerField()
+    volume = models.PositiveIntegerField()
+    cybersec_score = models.PositiveIntegerField()
