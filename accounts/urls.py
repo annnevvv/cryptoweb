8a82cab0901f266c97b73f910f61e95bab1dadb3
Anna Walaszek <noreplay@annn.evvv>
2023-11-12 17:03:07 +0100

    addes appneme varialbe to urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
+app_name = 'accounts'
+
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
