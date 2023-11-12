a3dee4ace9babd9d087fc014e8931a846a9081cd
Anna Walaszek <noreplay@annn.evvv>
2023-11-12 18:55:46 +0100

    added ryptocurrencyExchangeSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,1 +1,8 @@
 from rest_framework import serializers, permissions
+
+from homepage.models import CryptocurrencyExchange
+
+class CryptocurrencyExchangeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CryptocurrencyExchange
+        fields = '__all__'
