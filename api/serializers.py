dab7f9d0fcb7a3df287f574193a2502811cf1430
Anna Walaszek <noreplay@annn.evvv>
2023-11-12 22:31:43 +0100

    added serializer and view set

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers, permissions
 
-from homepage.models import CryptocurrencyExchange
+from homepage.models import CryptocurrencyExchange, Cryptocurrency
 
 class CryptocurrencyExchangeSerializer(serializers.ModelSerializer):
     class Meta:
         model = CryptocurrencyExchange
+        fields = '__all__'
+
+class CryptocurrencySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cryptocurrency
         fields = '__all__'
--- api/views.py
+++ api/views.py
@@ -2,10 +2,14 @@ from rest_framework import viewsets
 
 from django.shortcuts import render
 
-from homepage.models import CryptocurrencyExchange
-from .serializers import CryptocurrencyExchangeSerializer
+from homepage.models import CryptocurrencyExchange, Cryptocurrency
+from .serializers import CryptocurrencyExchangeSerializer, CryptocurrencySerializer
 
 
 class CryptocurrencyExchangeViewSet(viewsets.ModelViewSet):
     queryset = CryptocurrencyExchange.objects.all()
     serializer_class = CryptocurrencyExchangeSerializer
+
+class CryptocurrencyViewSet(viewsets.ModelViewSet):
+    queryset = Cryptocurrency.objects.all()
+    serializer_class = CryptocurrencySerializer
