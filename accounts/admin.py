7d068247aae7c6d6f6bb9b0bf1c691ce698b7ecb
Anna Walaszek <noreplay@annn.evvv>
2023-11-12 23:44:43 +0100

    added signal to auto-create user profile

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import UserAccountProfile
 
 admin.site.register(UserAccountProfile)
-# class UserProfileInline(admin.StackedInline):
-#     model = UserAccountProfile
-#     can_delete = False
+class UserAccountProfileInline(admin.StackedInline):
+    model = UserAccountProfile
+    can_delete = False
+
