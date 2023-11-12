0d91ecb4e947482d4161fe57c33c2a305b89e668
Anna Walaszek <noreplay@annn.evvv>
2023-11-12 21:55:12 +0100

    added cryptocurrency model to admin panel

--- homepage/admin.py
+++ homepage/admin.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 
-from.models import CryptocurrencyExchange
+from.models import CryptocurrencyExchange, Cryptocurrency
 
 admin.site.register(CryptocurrencyExchange)
 class CryptocurrencyExchangeAdmin(admin.ModelAdmin):
+    list_display = '__all__'
+
+class CryptocurrencyAdmin(admin.ModelAdmin):
     list_display = '__all__'
