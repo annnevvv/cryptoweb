48a00c19b76c086dd16b33249b2bdf5191614dbd
Anna Walaszek <noreplay@annn.evvv>
2023-11-12 18:58:24 +0100

    added api app to settings

--- cryptoweb/settings.py
+++ cryptoweb/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -44,10 +45,10 @@ INSTALLED_APPS = [
     # 'taggait',
     # 'rest_framework',
 
-    'homepage',
     'accounts',
+    'api',
+    'homepage',
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- cryptoweb/urls.py
+++ cryptoweb/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('accounts/', include('accounts.urls', 'accounts')),
+    path('api/', include('api.urls', 'api')),
     path('', include('homepage.urls', 'homepage')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
