d98dd24eb36a970275775f8742a42b4164995b33
Anna Walaszek <noreplay@annn.evvv>
2023-11-12 19:03:30 +0100

    added api serializer view to CryptocurrencyExchange

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,11 @@
+from rest_framework import routers
+
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+
+router = routers.DefaultRouter()
+router.register('api',)
 
 app_name = 'api'
 
--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
+from rest_framework import viewsets,
+
 from django.shortcuts import render
 
+from homepage.models import CryptocurrencyExchange
+from serializers import CryptocurrencyExchangeSerializer
 # Create your views here.
+
+class CryptocurrencyExchangeViewSet(viewsets.ModelViewSet):
+    queryset = CryptocurrencyExchange.objects.all()
+    serializer_class = CryptocurrencyExchangeSerializer
