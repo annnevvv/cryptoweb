f3442fd51206bbddbad10d80011f0515829e8e20
Anna Walaszek <noreplay@annn.evvv>
2023-11-26 19:48:10 +0100

    code do singup

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
-from .views import DashboardView
+from .views import DashboardView, SignupView
 
 app_name = 'accounts'
 
@@ -12,5 +12,6 @@ urlpatterns = [
     path("", include("django.contrib.auth.urls")),
     # path('login/', auth_views.LoginView.as_view(), name='login'),
     # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('signup', SignupView.as_view(), name='signup'),
     path('dashboard', DashboardView.as_view(), name='dashboard'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.contrib.auth.models import User
+from django.contrib.auth import login
 from django.views import View
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
 
 from .models import UserProfile, TransactionDiary
 
 # Create your views here.
+
+
+class SignupView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return super().form_valid(form)
 
 
 class DashboardView(View):
